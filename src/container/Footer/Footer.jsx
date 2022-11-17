1d977c2ea5608836e9cc885f2d7ff4df43e4ac5d
angelDova <Angelfcb01@yahoo.com>
2022-11-17 12:41:05 -0800

    third commit

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -11,12 +11,13 @@ const Footer = () => (
     <Newsletter />
 
     <div className="app__footer-links">
-      <div app__footer-links_contact>
+      <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
         <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
         <p className="p__opensans">+1 212-344-1230</p>
         <p className="p__opensans">+1 212-555-1230</p>
       </div>
+
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
         <p className="p__opensans">
@@ -25,7 +26,6 @@ const Footer = () => (
         </p>
         <img
           src={images.spoon}
-          alt="spoon"
           className="spoon__img"
           style={{ marginTop: 15 }}
         />
@@ -35,6 +35,7 @@ const Footer = () => (
           <FiInstagram />
         </div>
       </div>
+
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
@@ -43,8 +44,9 @@ const Footer = () => (
         <p className="p__opensans">07:00 am - 11:00 pm</p>
       </div>
     </div>
+
     <div className="footer__copyright">
-      <p className="p__opensans">2022 Gericht. All Rights Reserved</p>
+      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
     </div>
   </div>
 );
